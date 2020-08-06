b82a480def5582f55a2fd3b8d681a283d7cd0774
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-08-06 16:08:57 +0100

    Further attempts still.

--- application/models.py
+++ application/models.py
@@ -4,14 +4,14 @@ class Projects(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	projectName = db.Column(db.String(100), nullable=False, unique=True)
 	projectComplete = db.Column(db.Boolean, nullable=False, unique=False, default=False)
-	ustories = db.relationship('UserStories', backref='uStoryBR', lazy=True)
+	#ustories = db.relationship('UserStories', backref='uStoryBR', lazy=True)
 
 	def __repr__(self):
 		return ''.join([
 			'Project ID: ', self.id, '\r\n',
 			'Project Name: ', self.projectName, '\r\n'
 		])
-
+'''
 class UserStories(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	projectID = db.Column(db.Integer, db.ForeignKey('Projects.id'), nullable=False)
@@ -29,3 +29,4 @@ class UserStories(db.Model):
 			'I Want: ', self.ustoryIWant, '\r\n',
 			'To/So: ',  self.ustoryToSo, '\r\n'
 		])
+'''
